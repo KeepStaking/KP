ace2cebde984cf5e07d395806f5a9d0cae069554
beans <certifiedbagholder@gmail.com>
2023-01-24 12:49:59 -0800

    init

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import {GIGADRIP20} from "DRIP20/GIGADRIP20.sol";
+import {console} from "forge-std/console.sol";
 
 contract Counter {
     uint256 public number;
